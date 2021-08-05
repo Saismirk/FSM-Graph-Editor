fc3448cf9ad250759d3c30fa69441e26912d748a
Saismirk <ndgonzal@uwaterloo.ca>
2021-08-04 19:36:34 -0500

    Removed test behaviours

--- Assets/FSM/Behaviours/CombatBehaviour.cs
+++
@@ -1,9 +0,0 @@
-namespace FSM {
-    public class CombatBehaviour : StateBehaviour {
-        public float idlePoseChangeTimer = 2;
-        public override void OnUpdate(StateMachineRuntime stateMachine, StateMachineController stateMachineController, State state) {
-            stateMachineController.SetBool("detected", false);
-        }
-    }
-}
-
